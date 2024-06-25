547f5ab3673db2047462202a5c7e735612ee451c
Pedro F.Oliveira <fernandes.o.pedro@gmail.com>
2024-06-25 04:00:08 -0300

    GetConsumedFood Fix

--- src/service/consumed_food/get_all_consumed_food_service.ts
+++ src/service/consumed_food/get_all_consumed_food_service.ts
@@ -11,15 +11,22 @@ export class GetAllConsumedFoodService {
 
     async execute({userId, startDate, endDate}: ConsumedFoodUpdateRequest): Promise< ConsumedFood[] | Error >{
         
+        let start: Date = new Date(startDate);
+        let end: Date = new Date(endDate);
         const response = await prismaClient.consumedFood.findMany({
             where: { 
                 user_id:userId,
                 date:{
-                    lte:startDate,
-                    gte:endDate
+                    lte:start,
+                    gte:end
                 }
             }
         });
+
+        if(!response) {
+            console.log("Category does not exists");
+            return new Error("Category does not exists");
+        }
         
         return response;
     }
